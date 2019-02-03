ff301edb661cc55dd36ef8564d9949469e355ffe
Jorge <jorgecampoy81@gmail.com>
2019-02-03 14:00:37 +0100

    Created components: artist, home, search and navbar; Added some routes and HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-@NgModule({
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule( {
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
